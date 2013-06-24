8a04c3e83a9b4f06ad1f01f4ef63aed152309031
Adam Sven Johnson <adam@pkqk.net>
2013-06-24 23:38:56 +0100

    I'm really sory I'm one of those people*
    * the kind that make an icalendar feed from wikipedia
    instead of reading the rfc

--- eventbite.rb
+++ eventbite.rb
@@ -10,6 +10,10 @@ client = OAuth2::Client.new(
   :site => 'https://www.eventbrite.com'
 )
 
+get '/favicon.ico' do
+  410
+end
+
 get '/connect' do
   redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri)
 end
@@ -21,5 +25,40 @@ end
 
 get '/cal/:token' do
   ebc = EventbriteClient.new(:access_token => params[:token])
-  ebc.user_list_tickets
+
+  tickets = begin
+    r = ebc.user_list_tickets
+    r['user_tickets'][1]['orders'].map{|o| o['order']['event'] }
+  rescue
+    []
+  end
+
+  content_type 'text/calendar'
+  [200, ["BEGIN:VCALENDAR", "VERSION:2.0", *vevents(tickets), "END:VCALENDAR"].join("\n")]
+end
+
+def vevents(orders)
+  orders.map { |order| vevent(order) }
+end
+
+def vevent(event)
+  id = event['id']
+  title = event['title']
+  url = event['url']
+  start_date = ical_time(Time.parse(event['start_date']))
+  end_date = ical_time(Time.parse(event['end_date']))
+  [
+    "BEGIN:VEVENT",
+    "UID:#{id}@eventbrite.com",
+    "DTSTAMP:#{start_date}",
+    "DTSTART:#{start_date}",
+    "DTEND:#{end_date}",
+    "SUMMARY:#{title}",
+    "URL:#{url}",
+    "END:VEVENT"
+  ]
+end
+
+def ical_time(time)
+  time.utc.iso8601.gsub(/[:-]/,'')
 end
